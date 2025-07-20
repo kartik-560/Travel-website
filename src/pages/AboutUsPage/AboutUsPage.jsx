f894e59ef0273ba4bc559c9795f86145cda5d420
Kartik Kanzode <kartikkanzode@gmail.com>
2025-07-20 18:25:56 +0530

    Update AboutUsPage.jsx

--- src/pages/AboutUsPage/AboutUsPage.jsx
+++ src/pages/AboutUsPage/AboutUsPage.jsx
@@ -199,11 +199,13 @@ const AboutUsPage = () => {
               className="relative rounded-2xl overflow-hidden shadow-lg group bg-white"
             >
               {/* Always show image */}
-              <img
-                src={member.image}
-                alt={member.name}
-                className="w-full h-80 object-cover"
-              />
+             <div className="w-full aspect-square bg-white overflow-hidden rounded-t-2xl flex items-center justify-center">
+  <img
+    src={member.image}
+    alt={member.name}
+    className="max-w-full max-h-full object-contain"
+  />
+</div>
 
               {/* Mobile content: show always below image */}
               <div className="p-4 text-center md:hidden">
